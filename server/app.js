87d9ee3cde374950e77ee57d19633f10dfbc24fc
lvdat <lvdat13@gmail.com>
2022-12-08 07:35:59 +0700

    add body-parser, nodemon, edit scripts

--- server/app.js
+++ server/app.js
@@ -1,25 +1,26 @@
-const express = require("express");
-const path = require("path");
-const logger = require("morgan");
+const express = require("express")
+const path = require("path")
+const logger = require("morgan")
 require('dotenv').config()
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index")
+const usersRouter = require("./routes/users")
 
-const app = express();
+const app = express()
 
-app.use(logger("dev"));
-app.use(express.json());
+app.use(logger("dev"))
+app.use(express.json())
 app.use(
 	express.urlencoded({
 		extended: false,
 	})
-);
+)
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", indexRouter)
+app.use("/users", usersRouter)
 
 app.listen(SERVER_PORT, () => {
+	console.log(`> Server listening on port ${SERVER_PORT}`)
 
 })
 
