b56f1dee621e5474ead7b0af0e521d82c26c11d8
lvdat <lvdat13@gmail.com>
2022-12-08 07:24:15 +0700

    Remove: bin, public folder in server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
